70549894f585e342d2da72388e71ed2db8948ec1
Gupta <c2gupt@gmail.com>
2024-08-03 12:01:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css'
+import About from './components/AboutSection/About'
+import ContactPageOne from './components/Contacus/Contacts';
+import Hero from './components/HeroSection/Hero';
+// import Header from './components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+    {/* <Header/> */}
+    <Hero/>
+   <About/>
+   <ContactPageOne/>
+    
+    </>
+  )
+}
+
+export default App
